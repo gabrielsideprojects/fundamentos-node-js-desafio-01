0626c688897e906a0e86b06ff1d4c8f952336357
Gabriel <gabrielsideprojects@gmail.com>
2023-05-29 09:54:23 -0300

    Initial structure of the project

---
+++ src/routes.js
@@ -0,0 +1,61 @@
+import { Database } from './database.js'
+import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
+export const routes = [{
+    method: 'GET',
+    path:buildRoutePath('/users'),
+    handler: (req, res)=> {
+        const {search} = req.query
+        const users = database.select('users',search ?  {
+            name: search,
+            email: search
+        }: null)
+
+        return res.end(JSON.stringify(users))
+    }
+},
+{
+    method: 'POST',
+    path:buildRoutePath('/users'),
+    handler: (req, res)=> {
+        const {name, email} = req.body
+        const user = {
+            id: randomUUID(),
+            name,
+            email
+        }
+
+        database.insert('users', user)
+
+        return res.writeHead(201).end()
+    }
+},
+{
+    method: 'DELETE',
+    path:buildRoutePath('/users/:id'),
+    handler:(req,res)=> {
+        const { id } = req.params
+
+        database.delete('users', id)
+
+        return res.writeHead(204).end()
+        }
+},
+{
+    method: 'PUT',
+    path:buildRoutePath('/users/:id'),
+    handler:(req,res)=> {
+        const { id } = req.params
+        const { name, email} = req.body
+        database.update('users', id, {
+            name,
+            email
+        })
+
+        return res.writeHead(204).end()
+        }
+}
+]   
---
+++ src/utils/extract-query-params.js
@@ -0,0 +1,11 @@
+//search=Diego
+
+export function extractQueryParams(query) {
+    return query.substr(1).split('&').reduce((queryParams, param)=> {
+        const [key, value] = param.split('=')
+
+        queryParams[key] = value
+
+        return queryParams
+    }, {})
+}
